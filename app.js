04c3d5108969c5252b618b19350f2a7a1d4be047
Ignacio Iglesias <iiglesiascampoy@gmail.com>
2019-11-03 03:59:22 -0700

    version 1

--- app.js
+++ app.js
@@ -55,3 +55,92 @@ const loadLogin = function (){
         }
     })
 }
+
+const getProduct = function(index){
+    const products = [
+        {
+          imgUrl: "https://guesseu.scene7.com/is/image/GuessEU/M63H24W7JF0-L302-ALTGHOST?wid=1500&fmt=jpeg&qlt=80&op_sharpen=0&op_usm=1.0,1.0,5,0&iccEmbed=0",
+          name: "CHECK PRINT SHIRT",
+          price: 2103
+        },
+        {
+          imgUrl: "https://guesseu.scene7.com/is/image/GuessEU/FLGLO4FAL12-BEIBR?wid=700&amp;fmt=jpeg&amp;qlt=80&amp;op_sharpen=0&amp;op_usm=1.0,1.0,5,0&amp;iccEmbed=0",
+          name: "GLORIA HIGH LOGO SNEAKER",
+          price: 1740
+        },
+        {
+          imgUrl: "https://guesseu.scene7.com/is/image/GuessEU/HWVG6216060-TAN?wid=700&amp;fmt=jpeg&amp;qlt=80&amp;op_sharpen=0&amp;op_usm=1.0,1.0,5,0&amp;iccEmbed=0",
+          name: "CATE RIGID BAG",
+          price: 94.5
+        },
+        {
+          imgUrl: "http://guesseu.scene7.com/is/image/GuessEU/WC0001FMSWC-G5?wid=520&fmt=jpeg&qlt=80&op_sharpen=0&op_usm=1.0,1.0,5,0&iccEmbed=0",
+          name: "GUESS CONNECT WATCH",
+          price: 1810
+        },
+        {
+          imgUrl: "https://guesseu.scene7.com/is/image/GuessEU/AW6308VIS03-SAP?wid=700&amp;fmt=jpeg&amp;qlt=80&amp;op_sharpen=0&amp;op_usm=1.0,1.0,5,0&amp;iccEmbed=0",
+          name: "'70s RETRO GLAM KEFIAH",
+          price: 400
+        },
+        {
+          imgUrl:"https://http2.mlstatic.com/laptop-gamer-dell-g3-3779-intel-core-i7-8750h-16gb-1tb-ssd-128gb-pantalla-17-full-hd-nvidia-geforce-gtx-1050ti-4gb-D_NQ_NP_630765-MLM32008749861_082019-F.webp",
+          name:"LAPTOP GAMER DELL G3 3779 INTEL CORE I7 8750H 16GB 1TB SSD 128GB PANTALLA 17 FULL HD NVIDIA GEFORCE GTX 1050TI 4GB",
+          price: 28799
+        },
+        {
+          imgUrl:"https://http2.mlstatic.com/D_NQ_NP_2X_797508-MLA31274883489_062019-F.webp",
+          name:"XIAOMI MI A2 LITE DUAL SIM 64 GB NEGRO",
+          price: 3500
+        },
+        {
+          imgUrl:"https://http2.mlstatic.com/consola-playstation-4-ps4-slim-1tb-control-dualshock-4-D_NQ_NP_740091-MLM31236354023_062019-F.webp",
+          name:"CONSOLA PLAYSTATION 4 PS4 SLIM 1TB CONTROL DUALSHOCK 4",
+          price: 9600
+        },
+        {
+          imgUrl:"https://http2.mlstatic.com/vance-hines-twin-slash-harley-davidson-14-19-sportster-D_NQ_NP_894947-MLM31506376487_072019-F.webp",
+          name:"VANCE & HINES TWIN SLASH HARLEY DAVIDSON '14-'19 SPORTSTER",
+          price: 10120
+        },
+        {
+          imgUrl:"https://http2.mlstatic.com/D_NQ_NP_2X_734459-MLA31275670525_062019-F.webp",
+          name:"XIAOMI POCOPHONE F1 DUAL SIM 128 GB STEEL BLUE",
+          price: 10000
+        }
+      ]
+      return products[index]
+}
+
+const createImg = function(url) {
+	const productImg = document.createElement('img')
+	productImg.src = url
+	productImg.classList.add('image')
+	return productImg
+}
+const createName = function (name) {
+	const productName = document.createElement('h1')
+	const text = document.createTextNode(name)
+	productName.appendChild(text)
+	productName.classList.add('name')
+	return productName
+}
+const createPrice = function(price){
+    const productPrice = document.createElement('h2')
+    const text = document.createTextNode('$'+price)
+    productPrice.appendChild(text)
+    productPrice.classList.add('price')
+    return productPrice
+}
+
+const renderProduct = function(product) {
+    const screen = document.querySelector('#products')
+	screen.appendChild(createImg(product.imgUrl))
+    screen.appendChild(createName(product.name))
+    screen.appendChild(createPrice(product.price))
+}
+
+renderProduct(getProduct(0))
+renderProduct(getProduct(1))
+renderProduct(getProduct(2))
+renderProduct(getProduct(3))
